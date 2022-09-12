6b786a8dc7353031c09c45afd8f3589ef9be50df
CrioUser <crio-user@criodo.com>
2022-09-12 19:46:58 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = {backendEndpoint: "http://3.6.83.254:8081"};
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -2,8 +2,9 @@ import config from "../conf/index.js";
 
 async function init() {
   //Fetches list of all cities along with their images and description
+ 
   let cities = await fetchCities();
-
+  // console.log(cities)
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
@@ -14,14 +15,53 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try{
+    const response = await fetch("http://3.6.83.254:8082/cities");
+    // console.log(response, typeof(response)); 
+    const citiesData = await response.json();
+    // console.log(data, typeof(data)) //data is an array of objects
+    return citiesData //returns an array of objects with city data
+  }catch(err){
+    console.log(err)
+    return null
+  }
+  
 }
 
 //Implementation of DOM manipulation to add cities
-function addCityToDOM(id, city, description, image) {
+function addCityToDOM(id, city, description, image) 
+{
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  // const newCityCard = document.createElement('div')
+  // newCityCard.setAttribute('class', 'col-12 col-sm-6 col-lg-3 mb-4')
+  // const cardContentHTML = `
+  // <a href='pages/adventures/?city=${id}' id='${id}'>
+  //   <div class='tile'>
+  //     <div class='tile-text text-center'>
+  //       <h5>${city}</h5>
+  //       <p>${description}</p>
+  //     </div>
+  //     <img class='img-fluid' src='${image}'>
+  //   </div>
+  // </a>
+  // `
+  // newCityCard.innerHTML = cardContentHTML
+  // const citiesGridParentElement = document.getElementById('data')
+  // citiesGridParentElement.append(newCityCard)
+  const divele = document.createElement("div");
+  divele.setAttribute('class','col-12 col-sm-6 col-lg-3 mb-4');
+  divele.innerHTML = 
+  ` <a href='pages/adventures/?city=${id}' id='${id}'>
+  <div class='tile'>
+    <div class='tile-text text-center'>
+      <h5>${city}</h5>
+      <p>${description}</p>
+    </div>
+    <img class='img-fluid' src='${image}'>
+  </div>
+</a>`
 
+document.getElementById('data').appendChild(divele);
 }
-
 export { init, fetchCities, addCityToDOM };
